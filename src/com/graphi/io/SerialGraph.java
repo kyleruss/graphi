4b77ac347a1583167759fe808b8e9fb51b76dc87
denkers <kyle.business@inbox.com>
2015-12-15 21:10:25 +1300

    Added SerialGraph in io package that is an extension of SparseMultigraph and can be exported

---
+++ src/com/graphi/io/SerialGraph.java
@@ -0,0 +1,6 @@
+package com.graphi.io;
+
+import edu.uci.ics.jung.graph.SparseMultigraph;
+import java.io.Serializable;
+
+public class SerialGraph<V, E> extends SparseMultigraph<V, E> implements Serializable {}
