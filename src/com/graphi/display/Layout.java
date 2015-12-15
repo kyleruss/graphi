9f124215a4622bca3f80525a9f9ec109a545ac22
denkers <kyle.business@inbox.com>
2015-12-15 18:30:20 +1300

    Fixed issues with split in Layout, updated Layout with simulation controls

--- src/com/graphi/display/Layout.java
+++ src/com/graphi/display/Layout.java
@@ -2,10 +2,18 @@ package com.graphi.display;
 
 import com.graphi.test.GraphTest;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
+import edu.uci.ics.jung.visualization.control.ScalingControl;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
@@ -27,16 +35,39 @@ public class Layout extends JPanel
         
         splitPane.setLeftComponent(screenPanel);
         splitPane.setRightComponent(controlPanel); 
-        splitPane.setResizeWeight(0.7);
-        controlPanel.setMinimumSize(new Dimension(230, 650));
+       splitPane.setDividerLocation(670);
         add(splitPane, BorderLayout.CENTER);
     }
     
     private class ControlPanel extends JPanel
     {
+        private JPanel modePanel;
+        private JPanel simPanel;
+        
         public ControlPanel()
         {
-            setBackground(Color.WHITE);
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            setBorder(BorderFactory.createEmptyBorder(15, 0, 3, 8));
+            setMinimumSize(new Dimension(230, 650));
+            
+            modePanel   =   new JPanel();
+            simPanel    =   new JPanel();
+            modePanel.setBorder(BorderFactory.createTitledBorder("Mode controls"));
+            simPanel.setBorder(BorderFactory.createTitledBorder("Simulation controls"));
+  
+            ButtonGroup group       =   new ButtonGroup();
+            JCheckBox editCheck     =   new JCheckBox("Edit");
+            JCheckBox selectCheck   =   new JCheckBox("Select");
+            JCheckBox moveCheck     =   new JCheckBox("Move");
+            group.add(editCheck);
+            group.add(selectCheck);
+            group.add(moveCheck);
+            modePanel.add(editCheck);
+            modePanel.add(selectCheck);
+            modePanel.add(moveCheck);
+            
+            add(modePanel);
+            add(simPanel);
         }
     }
     
@@ -49,6 +80,7 @@ public class Layout extends JPanel
         public ScreenPanel()
         {            
             setLayout(new BorderLayout());
+            setBorder(BorderFactory.createEmptyBorder(15, 5, 5, 5));
             tabPane     =   new JTabbedPane();
             dataPanel   =   new DataPanel();
             graphPanel  =   new GraphPanel();
@@ -74,6 +106,9 @@ public class Layout extends JPanel
             {
                 setLayout(new CardLayout());
                 visViewer   =   GraphTest.getGraph();
+                ScalingControl scaler   =   new CrossoverScalingControl();
+                scaler.scale(visViewer, 0.7f, visViewer.getCenter());
+                visViewer.scaleToLayout(scaler);
                 visViewer.setBackground(Color.WHITE);
                 add(visViewer, "Test");
                 CardLayout cLayout  =   (CardLayout) getLayout();
