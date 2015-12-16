2c972c8f636c09f83f89b1e0e0dc2106ae6793dc
denkers <kyle.business@inbox.com>
2015-12-16 21:39:56 +1300

    Added kleinberg small world network generator in sim/Network

--- src/com/graphi/sim/Network.java
+++ src/com/graphi/sim/Network.java
@@ -1,8 +1,23 @@
 
 package com.graphi.sim;
 
+import com.graphi.test.Edge;
+import com.graphi.test.Node;
+import edu.uci.ics.jung.algorithms.generators.random.KleinbergSmallWorldGenerator;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
+import org.apache.commons.collections15.Factory;
+
 
 public class Network
 {
-    
+    public Graph<Node, Edge> generateKleinberg(int latticeSize, int clusterExp)
+    {
+        Factory<Node> nodeFactory                   =   () -> new Node();
+        Factory<Edge> edgeFactory                   =   () -> new Edge();
+        Factory<Graph<Node, Edge>> graphFactory     =   () -> new SparseMultigraph<>();   
+        
+        KleinbergSmallWorldGenerator gen            =   new KleinbergSmallWorldGenerator(graphFactory, nodeFactory, edgeFactory, latticeSize, clusterExp);
+        return gen.create();
+    }
 }
