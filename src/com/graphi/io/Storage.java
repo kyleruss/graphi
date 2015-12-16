e174ee1a11fce231c754dce9c27c5e72dff1fd62
denkers <kyle.business@inbox.com>
2015-12-16 20:00:44 +1300

    Added methods in io/Storage for opening & saving output/logs

--- src/com/graphi/io/Storage.java
+++ src/com/graphi/io/Storage.java
@@ -1,8 +1,12 @@
 
 package com.graphi.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -36,6 +40,38 @@ public class Storage
         {
             JOptionPane.showMessageDialog(null, "[Error] Failed to open graph");
             return null;
+        }
+    }
+    
+    public static void saveOutputLog(String logText, String file)
+    {
+        try(BufferedWriter bw   =   new BufferedWriter(new FileWriter(file)))
+        {
+            bw.write(logText);
+        }
+        
+        catch(IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "[Error] Failed to save output log");
+        }
+    }
+    
+    public static String openOutputLog(String file)
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader(file)))
+        {
+            String text =   "";
+            String line;
+            while((line = br.readLine()) != null)
+                text += line + "\n";
+            
+            return text;
+        }
+        
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "[Error] Failed to read output log");
+            return "";
         }
     }
 }
