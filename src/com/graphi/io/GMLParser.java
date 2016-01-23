0b298caf79acbe784d329efa658d5aa00c087447
denkers <kyle.business@inbox.com>
2016-01-23 14:40:17 +1300

    Added output body for exporting graph gml

--- src/com/graphi/io/GMLParser.java
+++ src/com/graphi/io/GMLParser.java
@@ -10,7 +10,12 @@ import com.graphi.util.Edge;
 import com.graphi.util.Node;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import javax.swing.JOptionPane;
 import org.apache.commons.collections15.Factory;
 
 
@@ -24,6 +29,41 @@ public class GMLParser
     
     public static void exportGraph(Graph<Node, Edge> graph, File file, boolean directed)
     {
+        String output   =   "";
         
+        //Graph
+        output   +=  "graph [\ndirected " + ((directed)? 1 : 0) + "\n";
+        
+        //Nodes
+        Collection<Node> nodes  =   graph.getVertices();
+        for(Node node : nodes)
+        {
+            output  +=  "node [\n";
+            output  +=  "id " + node.getID() + "\n";
+            output  +=  "label " + node.getName() + "\n]\n";
+        }
+        
+        //Edges
+        Collection<Edge> edges  =   graph.getEdges();
+        for(Edge edge : edges)
+        {
+            output  +=  "edge [\n";
+            output  +=  "source " + graph.getSource(edge).getID() + "\n";
+            output  +=  "target " + graph.getDest(edge).getID() + "\n";
+            output  +=  "weight " + edge.getWeight() + "\n]\n";
+        }
+        
+        //close graph
+        output      +=  "]";
+        
+        try(BufferedWriter writer   =   new BufferedWriter(new FileWriter(file)))
+        {
+            writer.write(output);
+        }
+        
+        catch(IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "Failed to write file");
+        }
     }
 }
