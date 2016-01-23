600d1b4a8409b420da620496acf38ef8f2e26a9e
denkers <kyle.business@inbox.com>
2016-01-23 13:43:35 +1300

    Added importGraph and exportGraph functions in io/GMLParser

--- src/com/graphi/io/GMLParser.java
+++ src/com/graphi/io/GMLParser.java
@@ -6,8 +6,24 @@
 
 package com.graphi.io;
 
+import com.graphi.util.Edge;
+import com.graphi.util.Node;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
+import java.io.File;
+import org.apache.commons.collections15.Factory;
+
 
 public class GMLParser 
 {
+    public static Graph<Node, Edge> importGraph(File file, Factory<Node> nodeFactory, Factory<Edge> edgeFactory)
+    {
+        Graph<Node, Edge> graph =   new SparseMultigraph<>();
+        return graph;
+    }
     
+    public static void exportGraph(Graph<Node, Edge> graph, File file, boolean directed)
+    {
+        
+    }
 }
