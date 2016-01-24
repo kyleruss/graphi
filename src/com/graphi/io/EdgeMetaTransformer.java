885264b66ed3ecee8ff14a1491d20a62a5e8c5af
denkers <kyle.business@inbox.com>
2016-01-25 12:54:11 +1300

    Added io/NodeMetaTransformer and io/EdgeMetaTransformer

---
+++ src/com/graphi/io/EdgeMetaTransformer.java
@@ -0,0 +1,15 @@
+
+package com.graphi.io;
+
+import com.graphi.util.Edge;
+import edu.uci.ics.jung.io.graphml.EdgeMetadata;
+import org.apache.commons.collections15.Transformer;
+
+
+public class EdgeMetaTransformer implements Transformer<EdgeMetadata, Edge>
+{
+    @Override
+    public Edge transform(EdgeMetadata i)
+    {
+    }
+}
