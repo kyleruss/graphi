0af92bf2051c211cf369e59da7d15eb21a221e05
denkers <kyle.business@inbox.com>
2016-01-25 13:01:26 +1300

    Added io/GraphMetaTransformer

--- src/com/graphi/io/EdgeMetaTransformer.java
+++ src/com/graphi/io/EdgeMetaTransformer.java
@@ -2,6 +2,7 @@
 package com.graphi.io;
 
 import com.graphi.util.Edge;
+import edu.uci.ics.jung.graph.util.EdgeType;
 import edu.uci.ics.jung.io.graphml.EdgeMetadata;
 import org.apache.commons.collections15.Transformer;
 
@@ -11,5 +12,7 @@ public class EdgeMetaTransformer implements Transformer<EdgeMetadata, Edge>
     @Override
     public Edge transform(EdgeMetadata i)
     {
+        Edge edge   =   new Edge(Integer.parseInt(i.getId()), i.isDirected()? EdgeType.DIRECTED : EdgeType.UNDIRECTED);
+        return edge;
     }
 }
