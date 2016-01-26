6179471a22219d77e274fada7c1896ef6ff7cba5
denkers <kyle.business@inbox.com>
2016-01-26 13:35:13 +1300

    Added io/PluginManager

---
+++ src/com/graphi/io/PluginManager.java
@@ -0,0 +1,13 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.io;
+
+
+public class PluginManager
+{
+    
+}
