2b85f43f4d50fcee11653ad5bc9d88bd5813064c
denkers <kyle.business@inbox.com>
2016-01-27 15:22:43 +1300

    Added io/Plugin interface

---
+++ src/com/graphi/io/Plugin.java
@@ -0,0 +1,14 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.io;
+
+public interface Plugin
+{
+    public String getPluginName();
+    
+    public void load();
+}
