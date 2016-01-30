76dfa964aec9d83a3ce418c9c702ae6c05d7d6fc
denkers <kyle.business@inbox.com>
2016-01-30 15:13:42 +1300

    Added plugins/st/StalkingPlugin and plugins/st/StalkingLayout

--- src/com/graphi/plugins/st/StalkingLayout.java
+++ src/com/graphi/plugins/st/StalkingLayout.java
@@ -4,17 +4,18 @@
 //  https://github.com/denkers/graphi
 //=========================================
 
-package com.graphi.plugins.def;
+package com.graphi.plugins.st;
 
 import com.graphi.display.LayoutPanel;
 import com.graphi.display.MainMenu;
 import javax.swing.JFrame;
 
 
-public class DefaultLayout extends LayoutPanel
+public class StalkingLayout extends LayoutPanel
 {
-    public DefaultLayout(MainMenu menu, JFrame frame) 
+    public StalkingLayout(MainMenu menu, JFrame frame) 
     {
         super(menu, frame);
     }
+    
 }
---
+++ src/com/graphi/plugins/st/StalkingPlugin.java
@@ -0,0 +1,20 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.plugins.st;
+
+import com.graphi.display.LayoutPanel;
+import com.graphi.plugins.AbstractPlugin;
+
+
+public class StalkingPlugin extends AbstractPlugin
+{
+    public StalkingPlugin(String name, String description, LayoutPanel panel) 
+    {
+        super(name, description, panel);
+    }
+    
+}
