8a13600d87ed6276749ff20fc1e68d634f872a3f
denkers <kyle.business@inbox.com>
2016-01-30 14:53:29 +1300

    Added plugins/def package and plugins/def/DefaultPlugin

---
+++ src/com/graphi/plugins/def/DefaultPlugin.java
@@ -0,0 +1,20 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.plugins.def;
+
+import com.graphi.display.LayoutPanel;
+import com.graphi.plugins.AbstractPlugin;
+
+
+public class DefaultPlugin extends AbstractPlugin
+{
+    public DefaultPlugin(String name, String description, LayoutPanel panel) 
+    {
+        super(name, description, panel);
+    }
+    
+}
