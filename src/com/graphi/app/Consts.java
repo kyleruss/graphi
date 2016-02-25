07e9fdb2e255a5ae09f7d9365fc37b802a61a5e7
denkers <kyle.business@inbox.com>
2016-02-25 18:51:28 +1300

    Added app/Consts

---
+++ src/com/graphi/app/Consts.java
@@ -0,0 +1,13 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.app;
+
+
+public final class Consts 
+{
+    private Consts() {}
+}
