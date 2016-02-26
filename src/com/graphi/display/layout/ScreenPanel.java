e24fe539bd35081a1145ffce72d54e5dbda36cdb
denkers <kyle.business@inbox.com>
2016-02-26 19:34:38 +1300

    Added layout/ScreenPanel

---
+++ src/com/graphi/display/layout/ScreenPanel.java
@@ -0,0 +1,55 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.display.layout;
+
+import java.awt.BorderLayout;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class ScreenPanel extends JPanel
+{
+    protected MainPanel mainPanel;
+    protected final MainPanel.ScreenPanel.DataPanel dataPanel;
+    protected final MainPanel.ScreenPanel.GraphPanel graphPanel;
+    protected final OutputPanel outputPanel;
+    protected final JTabbedPane tabPane;
+
+    public ScreenPanel(MainPanel mainPanel)
+    {            
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(15, 5, 5, 5));
+        
+        this.mainPanel      =   mainPanel;
+        tabPane             =   new JTabbedPane();
+        dataPanel           =   new MainPanel.ScreenPanel.DataPanel();
+        graphPanel          =   new MainPanel.ScreenPanel.GraphPanel();
+        outputPanel         =   new OutputPanel();
+
+
+        tabPane.addTab("", graphPanel);
+        tabPane.addTab("", dataPanel);
+        tabPane.addTab("", outputPanel);
+
+        JLabel dispLabel    =   new JLabel("Display", JLabel.CENTER);
+        JLabel dataLabel    =   new JLabel("Data", JLabel.CENTER);
+        JLabel outLabel     =   new JLabel("Output", JLabel.CENTER);
+
+        dispLabel.setIcon(new ImageIcon(mainPanel.graphIcon));
+        outLabel.setIcon(new ImageIcon(mainPanel.clipIcon));
+        dataLabel.setIcon(new ImageIcon(mainPanel.tableIcon));
+
+        tabPane.setTabComponentAt(0, dispLabel);
+        tabPane.setTabComponentAt(1, dataLabel);
+        tabPane.setTabComponentAt(2, outLabel);
+
+
+        add(tabPane);
+    }
+}
