0fa44c61137cf99bcac7230804ca24fdc210bcae
denkers <kyle.business@inbox.com>
2016-03-06 18:21:28 +1300

    Added javadocs in app/AppManager

--- src/com/graphi/app/AppManager.java
+++ src/com/graphi/app/AppManager.java
@@ -9,11 +9,18 @@ package com.graphi.app;
 import com.graphi.display.Window;
 import com.graphi.plugins.PluginManager;
 
+
+/**
+* Graphi entry point
+* Starts Graphi and initializes it's managers
+* Conceptually views Graphi plugins, config and display
+*/
+
 public final class AppManager 
 {
-    private final ConfigManager configManager;
-    private final PluginManager pluginManager;
-    private final Window window;
+    private final ConfigManager configManager; //Manages stored configs
+    private final PluginManager pluginManager; //Manages loading & active plugins
+    private final Window window; //Handles display of the application
     
     private AppManager()
     {
@@ -22,26 +29,42 @@ public final class AppManager
         pluginManager   =   new PluginManager(this);   
     }
     
+    /**
+    * @return The Graphi config manager that handles all stored configs
+    */
     public ConfigManager getConfigManager()
     {
         return configManager;
     }
     
+    /**
+    * @return The Graphi plugin manager for loading plugins & handeling active plugins
+    */
     public PluginManager getPluginManager()
     {
         return pluginManager;
     }
     
+    /**
+    * @return The Graphi layout/display manager
+    * Has access to the apps menu and frame
+    */
     public Window getWindow()
     {
         return window;
     }
     
+    /**
+    * @return Starts Graphi by initializing & displaying the applications frame
+    */
     private void start()
     {
         window.initFrame();
     }
     
+    /**
+     * @param args Unused
+     */
     public static void main(String[] args)
     {
         AppManager manager  =   new AppManager();
