3e3e5338a408609c610f26729590de0e426f1708
denkers <kyle.business@inbox.com>
2016-03-06 19:23:49 +1300

    Added javadocs to display/Window

--- src/com/graphi/display/Window.java
+++ src/com/graphi/display/Window.java
@@ -11,12 +11,17 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+/**
+ * Manages the Graphi menu and frame
+ * Uses singleton pattern: use the static getWindowInstance
+ * Initializes the applications look&feel package: LNF_PACKAGE
+ */
 
 public final class Window
 {
-    private final MainMenu menu;
-    private final JFrame frame;
-    private static Window instance;
+    private final MainMenu menu; //The applications frame menu bar (JMenuBar)
+    private final JFrame frame; //The active Graphi frame 
+    private static Window instance; //Only Window instance, see getWindowInstance() for initialization
     
     private Window()
     {
@@ -28,22 +33,34 @@ public final class Window
         initMenu();        
     }
     
+    /**
+    * @return The active Graphi frame
+    */
     public JFrame getFrame()
     {
         return frame;
     }
     
+    /**
+    * @return The app frame menu bar (JMenuBar)
+    */
     public MainMenu getMenu()
     {
         return menu;
     }
     
-    
+    /**
+    * Sets the frames menu bar to the initialized MainMenu: Window.menu 
+    */
     private void initMenu()
     {
         frame.setJMenuBar(menu);
     }
     
+    /**
+    * Sets the applications look & feel
+    * See Consts.LNF_PACKAGE for look & feel package
+    */
     private void initLookAndFeel()
     {
         try
@@ -53,11 +70,14 @@ public final class Window
         
         catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
         {
-            System.out.println("Error: " + e.getMessage());
-            e.printStackTrace();
+            System.out.println("[Error] Failed to initialize application look & feel: " + e.getMessage());
         }
     }
     
+    /**
+    * Initializes the application frame and displays it
+    * Initial plugin layout is already added to content pane by plugins/PluginManager
+    */
     public void initFrame()
     {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
@@ -66,6 +86,9 @@ public final class Window
         frame.setVisible(true);
     }
     
+    /**
+    * @return Creates a Window instance if it doesn't exist and returns the instance
+    */
     public static Window getWindowInstance()
     {
         if(instance == null)
