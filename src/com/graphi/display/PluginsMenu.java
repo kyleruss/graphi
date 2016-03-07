5e0335204f388c4eb69fce63dbf10ee0c19e8656
denkers <kyle.business@inbox.com>
2016-03-07 15:04:07 +1300

    Added javadocs in display/PluginsMenu

--- src/com/graphi/display/PluginsMenu.java
+++ src/com/graphi/display/PluginsMenu.java
@@ -17,10 +17,21 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
+/**
+ * A sub menu for display.MainMenu
+ * Contains options for importing plugins & a plugin list
+ * Loaded & active plugin names are maintained for later usage 
+ */
+
 public class PluginsMenu extends JMenu
 {
+    //The menu items of the current loaded plugins
     protected Map<String, JMenuItem> pluginMenuItems;
+    
+    //The active/current plugin menu item
     protected JMenuItem activePluginItem;
+    
+    //A tick icon used to identify the active plugin menu item
     protected BufferedImage activeTickIcon;
     
     public PluginsMenu()
@@ -30,10 +41,7 @@ public class PluginsMenu extends JMenu
         pluginMenuItems =   new HashMap<>();
         addPluginMenuItem("defaultPluginItem", new JMenuItem("Default"));
         
-        try
-        {
-            activeTickIcon  =   ImageIO.read(new File("resources/images/tick.png"));
-        }
+        try { activeTickIcon  =   ImageIO.read(new File("resources/images/tick.png")); }
         
         catch(IOException e)
         {
@@ -41,6 +49,11 @@ public class PluginsMenu extends JMenu
         }
     }
     
+    /**
+     * Adds plugin menu item to the menu and to the pluginMenuItems map
+     * @param name Name of the plugin or key of the plugin menu item in map
+     * @param item An item to add to the menu
+     */
     public void addPluginMenuItem(String name, JMenuItem item)
     {
         pluginMenuItems.put(name, item);
@@ -48,16 +61,32 @@ public class PluginsMenu extends JMenu
         revalidate();
     }
     
+    /**
+     * Returns the plugin menu items map where each entry key is
+     * the plugin name & the value is the plugins added JMenuItem
+     * @return A map containing plugin menu items
+     */
     public Map<String, JMenuItem> getPluginMenuItems()
     {
         return pluginMenuItems;
     }
     
+    /**
+     * Returns a menu item for the plugin name
+     * @param name The plugin name or key in the plugin menu items map used in addPluginMenuItem()
+     * @return The JMenuItem for the plugin if found; otherwise null
+     */
     public JMenuItem getPluginMenuItem(String name)
     {
         return pluginMenuItems.get(name);
     }
     
+    /**
+     * Makes the passed item the active/current plguin item
+     * The same plugin item cannot be reactivated and displays an error if this happens
+     * Sets activePluginItem to the item and changes the active tick icon to item
+     * @param item The item to make active - should not already be active
+     */
     public void setActivePluginItem(JMenuItem item)
     {
         if(activePluginItem != null)
