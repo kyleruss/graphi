a86c69210160b60bd60308b7a72a9e50a27ac266
denkers <kyle.business@inbox.com>
2016-03-09 22:15:56 +1300

    Added error/ErrorStub

--- src/com/graphi/error/ErrorDump.java
+++ src/com/graphi/error/ErrorDump.java
@@ -1,7 +1,14 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
 
 package com.graphi.error;
 
+import java.io.Serializable;
 
-public class ErrorDump {
+public class ErrorDump implements Serializable
+{
     
 }
---
+++ src/com/graphi/error/ErrorStub.java
@@ -0,0 +1,50 @@
+
+package com.graphi.error;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class ErrorStub implements Serializable
+{
+    private Date errorTime;
+    private Exception exception;
+    private String message;
+    
+    public ErrorStub(Date errorTime, Exception exception, String message)
+    {
+        this.errorTime  =   errorTime;
+        this.exception  =   exception;
+        this.message    =   message;
+    }
+    
+    public Date getErrorTime() 
+    {
+        return errorTime;
+    }
+
+    public void setErrorTime(Date errorTime) 
+    {
+        this.errorTime = errorTime;
+    }
+
+    public Exception getException() 
+    {
+        return exception;
+    }
+
+    public void setException(Exception exception)
+    {
+        this.exception = exception;
+    }
+
+    public String getMessage() 
+    {
+        return message;
+    }
+
+    public void setMessage(String message) 
+    {
+        this.message = message;
+    }
+}
