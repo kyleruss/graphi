cfe74f74934668841aab66fc4df6bbbb563f5815
denkers <kyle.business@inbox.com>
2016-03-09 22:08:51 +1300

    Added error/ExceptionHandler

---
+++ src/com/graphi/error/ExceptionHandler.java
@@ -0,0 +1,12 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.error;
+
+public class ExceptionHandler 
+{
+    
+}
