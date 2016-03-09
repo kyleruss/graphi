638062fb1a9329cf862383b6eef07d8d82dd7d54
denkers <kyle.business@inbox.com>
2016-03-09 21:06:16 +1300

    Added javadocs to io/NodeIDTransformer & io/Storage

--- src/com/graphi/io/NodeIDTransformer.java
+++ src/com/graphi/io/NodeIDTransformer.java
@@ -9,11 +9,19 @@ package com.graphi.io;
 import com.graphi.util.Node;
 import org.apache.commons.collections15.Transformer;
 
+/**
+ * A simple transformer to transform Node's to their ID string
+ */
 public class NodeIDTransformer implements Transformer<Node, String>
 {
+    /**
+     * Transforms a Node into a String representation of it's ID
+     * @param v The node to transform
+     * @return The String of the passed Node ID
+     */
     @Override
-    public String transform(Node i) 
+    public String transform(Node v) 
     {
-        return "" + i.getID();
+        return "" + v.getID();
     }
 }
--- src/com/graphi/io/Storage.java
+++ src/com/graphi/io/Storage.java
@@ -18,9 +18,17 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import javax.swing.JOptionPane;
 
-
+/**
+ * A utility class for various io operations
+ * Includes methods for importing/exporting serialized objects and log files
+ */
 public class Storage
 {
+    /**
+     * Exports the serialized object to file
+     * @param obj The object to export. Must implement Serializable interface
+     * @param file The file to export the object to
+     */
     public static void saveObj(Object obj, File file)
     {
         try(ObjectOutputStream oos  =   new ObjectOutputStream(new FileOutputStream(file)))
@@ -34,6 +42,11 @@ public class Storage
         }
     }
     
+    /**
+     * Imports an object from file
+     * @param file The file to import the object from
+     * @return The object read from the file
+     */
     public static Object openObj(File file)
     {
         try(ObjectInputStream ois   =   new ObjectInputStream(new FileInputStream(file)))
@@ -48,6 +61,11 @@ public class Storage
         }
     }
     
+    /**
+     * Saves the passed text to file
+     * @param logText The text to write to file
+     * @param file The file to save the text to
+     */
     public static void saveOutputLog(String logText, File file)
     {
         try(BufferedWriter bw   =   new BufferedWriter(new FileWriter(file)))
@@ -61,6 +79,11 @@ public class Storage
         }
     }
     
+    /**
+     * Reads & returns the text contained in the file
+     * @param file The file to get the containing text
+     * @return The text read from the file
+     */
     public static String openOutputLog(File file)
     {
         try(BufferedReader br = new BufferedReader(new FileReader(file)))
