74434b2859052b5d869c8632550fcb72a4e11731
denkers <kyle.business@inbox.com>
2016-03-12 22:55:20 +1300

    Added javadocs to plugins/Plugin

--- src/com/graphi/plugins/Plugin.java
+++ src/com/graphi/plugins/Plugin.java
@@ -10,17 +10,49 @@ import com.graphi.app.AppManager;
 import com.graphi.display.layout.MainPanel;
 import com.graphi.util.GraphData;
 
+/**
+ * An abstract definition for a plugin bean
+ * Implemenation should preserve name, description and panel fields
+ * Also should permit message passing between plugins
+ */
 public interface Plugin
 {
+    /**
+     * Return the produced or maintained  plugin name
+     * The plugin name should be distinguishable and relevant to the function of the plugin
+     * @return A String of the Plugin name 
+     */
     public String getPluginName();
     
+    /**
+     * Return the produced or maintained plugin description
+     * The plugin description briefly describes the function of the plugin
+     * @return A String of the Plugin description
+     */
     public String getPluginDescription();
     
+    /**
+     * Return a produced or maintained Layout for the plugin
+     * Initialized in attachPanel
+     * @return The plugins layout; Can return null 
+     */
     public MainPanel getPanel();
     
+    /**
+     * Responsible for initializing the Plugin's panel 
+     * @param appManager The parent AppManager passed to MainPanel constructor for attachment
+     */
     public void attachPanel(AppManager appManager);
     
+    /**
+     * Adds the GraphData to the Plugin's panel
+     * Allows the passing of graph data when changing plugins
+     * @param data 
+     */
     public void passData(GraphData data);
     
+    /**
+     * @return The Plugin panel's active graph data
+     */
     public GraphData getData();
 }
