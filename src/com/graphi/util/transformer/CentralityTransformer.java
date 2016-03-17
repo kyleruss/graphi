40fb60d0a601f91565d31d84390acbcac7cd83df
denkers <kyle.business@inbox.com>
2016-03-17 18:11:44 +1300

    Added javadocs to transformer/CentralityTransformer

--- src/com/graphi/util/transformer/CentralityTransformer.java
+++ src/com/graphi/util/transformer/CentralityTransformer.java
@@ -12,9 +12,16 @@ import java.awt.geom.Ellipse2D;
 import java.util.List;
 import org.apache.commons.collections15.Transformer;
 
+/**
+ * A transformer for scaling node size based on their rank in 
+ * a vector from a centrality computation (see util.MatrixTools)
+ */
 public class CentralityTransformer implements Transformer<Node, Shape>
 {
+    //A list of nodes in order of their rank in the graph
     List<Node> centralNodes;
+    
+    //The number of ranks to consider scaling size before normalizing the remaining node size
     int numRanks;
 
     public CentralityTransformer(List<Node> centralNodes, int numRanks)
@@ -23,6 +30,12 @@ public class CentralityTransformer implements Transformer<Node, Shape>
         this.numRanks       =   numRanks;
     }
 
+    /**
+     * Scales a node's size based on it's rank in the vector
+     * Scales the size of up to numRanks many nodes
+     * @param node The Node's size to scale (should exist in the vector)
+     * @return A shape with size scaled appropriate to the passed node's rank in the vector
+     */
     @Override
     public Shape transform(Node node) 
     {
