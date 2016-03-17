4cb3e3df73365e69be561934416926f27b5764c8
denkers <kyle.business@inbox.com>
2016-03-17 19:40:26 +1300

    Added javadocs to transformer/EdgeLabelTransformer

--- src/com/graphi/util/transformer/EdgeLabelTransformer.java
+++ src/com/graphi/util/transformer/EdgeLabelTransformer.java
@@ -10,8 +10,12 @@ import com.graphi.util.Edge;
 import java.text.DecimalFormat;
 import org.apache.commons.collections15.Transformer;
 
+/**
+ * A simple transformer for the purpose of displaying Edge labels (Edge weight)
+ */
 public class EdgeLabelTransformer implements Transformer<Edge, String>
 {
+    //A flag to display or hide edge labels
     private boolean showLabels;
 
     public EdgeLabelTransformer(boolean showLabels)
@@ -19,6 +23,11 @@ public class EdgeLabelTransformer implements Transformer<Edge, String>
         this.showLabels   =   showLabels;
     }
 
+    /**
+     * Transforms the Edge into it's a formatted edge weight
+     * @param edge The Edge to transform
+     * @return A formatted String of the passed edge's weight; An empty String if showLabels is false
+     */
     @Override
     public String transform(Edge edge)
     {
@@ -31,11 +40,18 @@ public class EdgeLabelTransformer implements Transformer<Edge, String>
         else return "";
     }
     
+    /**
+     * Sets the showLabels flag to the passed showLabels
+     * @param showLabels True then Edge labels are shown; otherwise they are hidden
+     */
     public void showLabels(boolean showLabels)
     {
         this.showLabels =   showLabels;
     }
     
+    /**
+     * @return The showLabels flag. True if the labels are to be shown; false if they are hidden
+     */
     public boolean isShowingLabels()
     {
         return showLabels;
