0f41bd1eeb6e98a9e70be51aa3b605247ea59023
denkers <kyle.business@inbox.com>
2016-03-17 22:58:12 +1300

    Added javadocs to transformer/VertexLabelTransformer

--- src/com/graphi/util/transformer/VertexLabelTransformer.java
+++ src/com/graphi/util/transformer/VertexLabelTransformer.java
@@ -9,8 +9,12 @@ package com.graphi.util.transformer;
 import com.graphi.util.Node;
 import org.apache.commons.collections15.Transformer;
 
+/**
+ * A simple transformer for transforming Node's into their name labels
+ */
 public class VertexLabelTransformer implements Transformer<Node, String>
 {
+    //A flag indicating the display of Node labels
     private boolean showLabels;
     
     public VertexLabelTransformer(boolean showLabels)
@@ -18,6 +22,11 @@ public class VertexLabelTransformer implements Transformer<Node, String>
         this.showLabels =   showLabels;
     }
 
+    /**
+     * Transforms a node into a String that is it's name
+     * @param node The Node to transform
+     * @return if the showLabels flag is true then returns the passed Node's name; otherwise empty String
+     */
     @Override
     public String transform(Node node)
     {
@@ -27,11 +36,17 @@ public class VertexLabelTransformer implements Transformer<Node, String>
             return "";
     }
     
+    /**
+     * @return The showLabels flag indiciating if the vertex labels are visible
+     */
     public boolean isShowingLabels()
     {
         return showLabels;
     }
     
+    /**
+     * @param showLabels Pass true to show labels; false to hide
+     */
     public void showLabels(boolean showLabels)
     {
         this.showLabels =   showLabels;
