117322b75664a248bb78d13c69273abd38d9770e
denkers <kyle.business@inbox.com>
2016-05-08 21:11:39 +1200

    Added layout/AppResources

---
+++ src/com/graphi/display/layout/AppResources.java
@@ -0,0 +1,12 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.display.layout;
+
+public class AppResources 
+{
+    
+}
