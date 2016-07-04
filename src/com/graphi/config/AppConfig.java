3c602d5f7fff44bbe4e47739dde5d63572f9e382
denkers <kyle.business@inbox.com>
2016-07-04 20:35:43 +1200

    Added config.AppConfig

---
+++ src/com/graphi/config/AppConfig.java
@@ -0,0 +1,12 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.config;
+
+public class AppConfig 
+{
+    
+}
