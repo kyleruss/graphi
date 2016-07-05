59386fbd941201358593e00ee82cd0e532b7dc64
denkers <kyle.business@inbox.com>
2016-07-05 16:41:46 +1200

    Added tasks.AbstractTask

---
+++ src/com/graphi/tasks/AbstractTask.java
@@ -0,0 +1,44 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.tasks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractTask implements Task
+{
+    private String description;
+    private String name;
+    private Map properties;
+    
+    public AbstractTask()
+    {
+        description =   "";
+        name        =   "";
+        properties  =   new HashMap();
+    }
+    
+    @Override
+    public String getTaskDescription() 
+    {
+        return description;
+    }
+
+    @Override
+    public String getTaskName() 
+    {
+        return name;
+    }
+    
+    public Map getProperties()
+    {
+        return properties;
+    }
+
+    @Override
+    public abstract void performTask();
+}
