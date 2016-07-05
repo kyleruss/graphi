f3cc4811828cd965b5202c445c8c5bcc9f4ce632
denkers <kyle.business@inbox.com>
2016-07-05 16:34:41 +1200

    Added Task interface in  tasks.Task

---
+++ src/com/graphi/tasks/Task.java
@@ -0,0 +1,21 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.tasks;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public interface Task extends Serializable
+{
+    public String getTaskDescription();
+    
+    public String getTaskName();
+    
+    public void performTask();
+    
+    public Map getTaskProperties();
+}
