cf1dc9ed144ecb9137eb3f5a69fd53ad2d81af7c
denkers <kyle.business@inbox.com>
2016-07-06 20:25:05 +1200

    Added tasks.ClearEntriesTask

---
+++ src/com/graphi/tasks/ClearEntriesTask.java
@@ -0,0 +1,12 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.tasks;
+
+public class ClearEntriesTask 
+{
+    
+}
