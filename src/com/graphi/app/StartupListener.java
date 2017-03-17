d37d6b939e5975687f1527858124512367ce1e83
denkers <kyle.business@inbox.com>
2017-03-17 22:12:38 +1300

    Added app.StartupListener

---
+++ src/com/graphi/app/StartupListener.java
@@ -0,0 +1,41 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.app;
+
+import java.util.Map;
+
+public abstract class StartupListener 
+{
+    protected Map<String, String> params;
+    
+    protected abstract void initParamNames();
+    
+    protected void setInitialParamName(String paramName)
+    {
+        params.put(paramName, null);
+    }
+    
+    public Map<String, String> getParams()
+    {
+        return params;
+    }
+    
+    public boolean hasParam(String paramName)
+    {
+        return params.containsKey(paramName);
+    }
+    
+    public String getParam(String paramName)
+    {
+        return params.get(paramName);
+    }
+    
+    public void setParam(String paramName, String paramValue)
+    {
+        params.put(paramName, paramValue);
+    }
+}
