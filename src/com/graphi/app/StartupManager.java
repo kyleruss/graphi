20162428c12777c29e254c0f0f81893695d0d72d
denkers <kyle.business@inbox.com>
2017-03-17 22:48:56 +1300

    Added app.StartupManager

---
+++ src/com/graphi/app/StartupManager.java
@@ -0,0 +1,64 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StartupManager
+{
+    private static StartupManager instance;
+    private final List<StartupListener> listeners;
+    private final Map<String, String> appParams;
+    
+    private StartupManager(String[] params)
+    {
+        listeners       =   new ArrayList<>();
+        appParams       =   new HashMap<>();
+        initAppParams(params);
+    }
+    
+    private void initAppParams(String[] params)
+    {
+        if(params == null || params.length < 2) return;
+        
+        for(int paramIndex = 0; (paramIndex + 1) < params.length; paramIndex += 2)
+        {
+            String paramName    =   params[paramIndex];
+            String paramValue   =   params[paramIndex + 1];
+            appParams.put(paramName, paramValue);
+        }
+    }
+    
+    public void addListener(StartupListener listener)
+    {
+        listeners.add(listener);
+    }
+    
+    public Map<String, String> getAppParams()
+    {
+        return appParams;
+    }
+    
+    public List<StartupListener> getListeners()
+    {
+        return listeners;
+    }
+    
+    public static StartupManager createInstance(String[] appParams)
+    {
+        if(instance == null) instance   =   new StartupManager(appParams);
+        return instance;
+    }
+    
+    public static StartupManager getInstance()
+    {
+        return instance;
+    }
+}
