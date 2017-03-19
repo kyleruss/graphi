d9e79d4226857c018dda2b60770812a1ae604beb
denkers <kyle.business@inbox.com>
2017-03-19 23:00:40 +1300

    Added layout.TitlePanel

---
+++ src/com/graphi/display/layout/TitlePanel.java
@@ -0,0 +1,14 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.display.layout;
+
+import javax.swing.JPanel;
+
+public class TitlePanel extends JPanel
+{
+    
+}
