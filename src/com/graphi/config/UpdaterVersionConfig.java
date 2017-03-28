4de9cb45ab0b8ce1e1dbfa0387b4f8872fa2a79e
denkers <kyle.business@inbox.com>
2017-03-28 23:24:51 +1300

    Added config.UpdaterVersionConfig

---
+++ src/com/graphi/config/UpdaterVersionConfig.java
@@ -0,0 +1,54 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import org.w3c.dom.Document;
+
+public class UpdaterVersionConfig implements Config
+{
+    private String buildID;
+    private Date buildDate;
+    
+    
+    @Override
+    public void parseDocumentConfig(Document document) 
+    {
+        try
+        {
+            buildID                 =   ConfigManager.getStringConfig(document, "build-id");
+            String buildDateStr     =   ConfigManager.getStringConfig(document, "build-date");
+        
+            SimpleDateFormat dateFmt    =   new SimpleDateFormat("dd-MM-yyy");
+            buildDate                   =   dateFmt.parse(buildDateStr);
+        }
+        
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "[Error] Failed to read version config");
+        }
+    }
+
+    public String getBuildID()
+    {
+        return buildID;
+    }
+    
+    public Date getBuildDate()
+    {
+        return buildDate;
+    }
+    
+    @Override
+    public String getConfigName()
+    {
+        return "version";
+    }
+}
