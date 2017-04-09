b0cf3b41032d3de9dcfece6435752e0cb41b7ce7
denkers <kyle.business@inbox.com>
2017-04-09 16:35:54 +1200

    Added controls.options.CustomizationOptionPanel

---
+++ src/com/graphi/display/layout/controls/options/CustomizationOptionPanel.java
@@ -0,0 +1,29 @@
+//=========================================
+//  Kyle Russell
+//  AUT University 2015
+//  https://github.com/denkers/graphi
+//=========================================
+
+package com.graphi.display.layout.controls.options;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+
+public class CustomizationOptionPanel extends AbstractOptionPanel
+{
+    private JCheckBox enableResCheck;
+    private JSpinner customWidthSpinner;
+    private JSpinner customHeightSpinner;
+    private JComboBox themeBox;
+    private JButton displayBGButton;
+    private JButton nodeBGButton;
+    private JButton edgeBGButton;
+    private JComboBox edgeTypeBox;
+    
+    public CustomizationOptionPanel()
+    {
+        
+    }
+}
