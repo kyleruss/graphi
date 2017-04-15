dc2c9e4ac1c2b1ef2705f389c40a1d5c2a557bdf
denkers <kyle.business@inbox.com>
2017-04-15 21:33:41 +1200

    Updated io.ProjectStore with importing/exporting of graphi projects

--- src/com/graphi/io/ProjectStore.java
+++ src/com/graphi/io/ProjectStore.java
@@ -7,14 +7,59 @@
 package com.graphi.io;
 
 import com.graphi.graph.GraphData;
+import java.io.File;
+import java.io.Serializable;
+import javax.swing.JFileChooser;
 import javax.swing.JTable;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class ProjectStore 
+public class ProjectStore implements Serializable
 {
     private GraphData graphData;
     private String output;
-    private JTable outputTable;
-
+    private JTable computeTable;
+    
+    public ProjectStore()
+    {
+        this(null, null, null);
+    }
+    
+    public ProjectStore(GraphData graphData, String output, JTable computeTable)
+    {
+        this.graphData          =   graphData;
+        this.output             =   output;
+        this.computeTable       =   computeTable;
+    }
+    
+    public void exportProject()
+    {
+        File file   =   getFile(false);
+        
+        if(file != null)
+            Storage.saveObj(this, file);
+    }
+    
+    
+    public static ProjectStore importProject()
+    {
+        File file               =   getFile(true);
+        ProjectStore instance   =   (ProjectStore) Storage.openObj(file, null);
+        
+        return instance;
+    }
+    
+    private static File getFile(boolean open)
+    {
+        JFileChooser fileChooser        =   new JFileChooser();
+        FileNameExtensionFilter filter  =   new FileNameExtensionFilter("Graphi project file", "gproject");    
+        int option                      =   open? fileChooser.showOpenDialog(null) : fileChooser.showSaveDialog(null);
+        
+        if(option == JFileChooser.APPROVE_OPTION)
+            return fileChooser.getSelectedFile();
+        else 
+            return null;
+    }
+    
     public GraphData getGraphData() 
     {
         return graphData;
@@ -35,14 +80,14 @@ public class ProjectStore
         this.output = output;
     }
 
-    public JTable getOutputTable() 
+    public JTable getComputeTable() 
     {
-        return outputTable;
+        return computeTable;
     }
 
-    public void setOutputTable(JTable outputTable) 
+    public void setComputeTable(JTable computeTable) 
     {
-        this.outputTable = outputTable;
+        this.computeTable = computeTable;
     }
     
     
